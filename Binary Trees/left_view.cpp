005a2f4b516032b087ad1c5ba413674f7d4fd158
parth121101 <96720723+parth121101@users.noreply.github.com>
2023-04-13 22:05:06 +0530

    add binary trees left view

---
+++ Binary Trees/left_view.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+//Function to return a list containing elements of left view of the binary tree.
+vector<int> leftView(Node *root)
+{
+    vector<int> s;
+    if (!root){return s;}
+ 
+    queue<Node*> q;
+    q.push(root);
+ 
+    while (!q.empty())
+    {    
+        // number of nodes at current level
+        int n = q.size();
+         
+        // Traverse all nodes of current level
+        for(int i = 1; i <= n; i++)
+        {
+            Node* temp = q.front();
+            q.pop();
+                 
+            // Print the left most element
+            // at the level
+            if (i == 1)
+                s.push_back(temp->data);
+             
+            // Add left node to queue
+            if (temp->left != NULL)
+                q.push(temp->left);
+ 
+            // Add right node to queue
+            if (temp->right != NULL)
+                q.push(temp->right);
+        }
+    }
+    return s;
+}
